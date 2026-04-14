9d15ae4254041fff05a19a4daec3c8cdf3177fd0
Glen O'Connor <glenoconnor2026@icloud.com>
2026-04-14 10:26:51 +1000

    Initial Commit

---
+++ SnacktacularUI/SnacktacularUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularUIApp.swift
+//  SnacktacularUI
+//
+//  Created by Glen O'Connor on 14/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
